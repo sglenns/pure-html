78a6f657f5eacb165cc974ef1f020e7e12b17c75
sglenns <gsm1984@gmail.com>
2025-08-03 13:34:12 +0200

    Fix SEO and format whole project

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
-import { fileURLToPath, URL } from "node:url";
-
-import { defineConfig } from "vite";
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-});
+})
