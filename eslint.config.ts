33fa8eec0bac85608b4e6cb6758cba4f29da2d8a
sglenns <gsm1984@gmail.com>
2025-08-19 20:56:50 +0200

    Allow lint of md files

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,33 +1,41 @@
-import css from '@eslint/css'
-import js from '@eslint/js'
-import markdown from '@eslint/markdown'
-import eslintConfigPrettier from 'eslint-config-prettier/flat'
-import { defineConfig, globalIgnores } from 'eslint/config'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
+import css from "@eslint/css";
+import js from "@eslint/js";
+import markdown from "@eslint/markdown";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import { defineConfig, globalIgnores } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default defineConfig([
-  globalIgnores(['dist/**/*']),
+  globalIgnores(["dist/**/*"]),
   eslintConfigPrettier,
   tseslint.configs.recommended,
-  markdown.configs.recommended,
   {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    files: ["**/*.md"],
+    plugins: { markdown },
+    extends: ["markdown/recommended"],
+    language: "markdown/gfm",
+    rules: {
+      "markdown/no-html": "error",
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     plugins: { js },
-    extends: ['js/recommended'],
+    extends: ["js/recommended"],
     languageOptions: { globals: globals.browser },
   },
   {
-    files: ['**/*.css'],
+    files: ["**/*.css"],
     plugins: { css },
-    language: 'css/css',
+    language: "css/css",
     languageOptions: { tolerant: true },
-    extends: ['css/recommended'],
+    extends: ["css/recommended"],
     rules: {
-      'css/no-invalid-at-rules': 'error',
-      'css/use-baseline': ['warn', { available: 'widely' }],
-      'css/no-invalid-properties': ['error', { allowUnknownVariables: true }],
-      'css/no-empty-blocks': 'error',
+      "css/no-invalid-at-rules": "error",
+      "css/use-baseline": ["warn", { available: "widely" }],
+      "css/no-invalid-properties": ["error", { allowUnknownVariables: true }],
+      "css/no-empty-blocks": "error",
     },
   },
-])
+]);
