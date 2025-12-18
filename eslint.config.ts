429ae8b3b95fdb9c32f9d1bac777ff3f702ef21b
Glenn Mendoza <g.mendoza@nephila.digital>
2025-12-18 17:24:05 +0100

    feat: add HTML linting

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,13 +1,15 @@
 import css from "@eslint/css";
 import js from "@eslint/js";
 import markdown from "@eslint/markdown";
+import html from "@html-eslint/eslint-plugin";
+import htmlParser from "@html-eslint/parser";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default defineConfig([
-  globalIgnores(["dist/**/*"]),
+  globalIgnores(["dist/**/*", "node_modules/**/*"]),
   eslintConfigPrettier,
   tseslint.configs.recommended,
   {
@@ -19,12 +21,31 @@ export default defineConfig([
       "markdown/no-html": ["error"],
     },
   },
+
+  {
+    ...html.configs?.["flat/recommended"],
+    files: ["**/*.html"],
+    plugins: { "@html-eslint": html },
+    languageOptions: {
+      parser: htmlParser,
+      parserOptions: {
+        templateEngineSyntax: {
+          "{{": "}}",
+        },
+      },
+    },
+    rules: {
+      "@html-eslint/use-baseline": ["warn", { available: "widely" }],
+    },
+  },
+
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     plugins: { js },
     extends: ["js/recommended"],
     languageOptions: { globals: globals.browser },
   },
+
   {
     files: ["**/*.css"],
     plugins: { css },
@@ -32,10 +53,10 @@ export default defineConfig([
     languageOptions: { tolerant: true },
     extends: ["css/recommended"],
     rules: {
-      "css/no-invalid-at-rules":[ "error"],
+      "css/no-invalid-at-rules": ["error"],
       "css/use-baseline": ["warn", { available: "widely" }],
       "css/no-invalid-properties": ["error", { allowUnknownVariables: true }],
-      "css/no-empty-blocks":[ "error"],
+      "css/no-empty-blocks": ["error"],
     },
   },
 ]);
