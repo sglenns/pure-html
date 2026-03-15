674c64308d2f61da991d50e729ca557483af0827
sglenns <gsm1984@gmail.com>
2026-03-15 20:00:42 +0100

    fix: gemini code review bugs

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,3 @@ import "@fontsource-variable/roboto";
 import "@/scripts/dialog";
 
 import "@/styles/index.css";
-
-document.getElementById("open-dialog-btn")?.addEventListener("click", () => {
-  (document.getElementById("dialog-demo") as HTMLDialogElement)?.showModal();
-});
